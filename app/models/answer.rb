9f7dae8275e849bf0d0e5c92295f7a04a84150a9
tzzzoz <tzzzoz@gmail.com>
2011-08-16 14:58:58 +0800

    handle credits when create answer paid questions

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,13 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question, :counter_cache => true
   has_many :votes
+  
+  validate :enough_credit_to_answer, :on => :create
+  
+  def enough_credit_to_answer
+    if self.user.credit < APP_CONFIG["answer_paid_question"]
+      errors.add_to_base("You do NOT have enough credit to answer this paid question! Please earn more credits!")
+    end
+  end
+  
 end
