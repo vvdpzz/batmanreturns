986cde19dc4e75f35d8085a57b665bf08ff70f7d
VvDPzZ <vvdpzz@gmail.com>
2011-08-18 15:07:28 +0800

    remove link

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,12 +46,29 @@
 #   
 # end
 
+require 'open-uri'
+
+class String
+  def is_numeric?
+    Float self rescue false
+  end
+end
+
+
+titleRedis = Redis.new(:host => '192.168.1.141', :port => 6379)
+
 urls = []
 
-url = "http://bbs.55bbs.com/forum-8-1.html"
-doc = Nokogiri::HTML(open(url))
-doc.css("th span a").each do |item|  
-  urls << item[:href]
+20.times do |i|
+  url = "http://bbs.55bbs.com/forum-8-#{i+1}.html"
+  doc = Nokogiri::HTML(open(url))
+  doc.css("th span a").each do |item|  
+    urls << item[:href]
+    if not item.text.is_numeric?
+      titleRedis.set(item[:href].split('-')[1], item.text)
+      puts item[:href].split('-')[1]
+    end
+  end
 end
 
 i = 0
@@ -64,33 +81,58 @@ end
 urls.compact!
 urls.each do |url|
   a = url.split('-')
-  redis.zadd(a[1],a[2],a[2])
+  $redis.zadd(a[1],a[2],a[2])
 end
 
-redis.keys.each do |key|
-  a = redis.zrangebyscore(key, '-inf', '+inf')
+puts "------------------------------------------------------------"
+
+$redis.keys.each do |key|
+  a = $redis.zrangebyscore(key, '-inf', '+inf')
   if a[0] != a[-1]
-    puts "thread-#{key}-#{a[0]}-1.html"
-    puts "thread-#{key}-#{a[-1]}-1.html"
-    
+        
     s = a[0].to_i
     e = a[-1].to_i
     i=s
+    question_id = 0
     while i<= e do
       url = "thread-#{key}-#{i}-1.html"
       page = Nokogiri::HTML(open("http://bbs.55bbs.com/" + url))
+      count = 0
+      title = titleRedis.get(key)
       page.css(".t_msgfont").each do |item|
-        puts item.text
+        count+=1
+        if i==a[0].to_i and count ==1
+          question = Question.create(:title => title, :content => item.text, :user_id => 1)
+          question_id = question.id
+          puts "Create Question for #{question_id}"
+        else
+          Answer.create(:question_id => question_id, :user_id => 1, :content => item.text)
+          puts ".......... .......... add answer to #{question_id}"
+        end
       end
       i+=1
     end
   else
-    puts "thread-#{key}-#{a[0]}-1.html"
     url = "thread-#{key}-#{a[0]}-1.html"
     
     page = Nokogiri::HTML(open("http://bbs.55bbs.com/" + url))
+    count = 0
+    title = titleRedis.get(key)
+    puts "one"
+    puts key
+    puts title
+    question_id = 0
     page.css(".t_msgfont").each do |item|
-      puts item.text
+      count += 1
+      if count == 1
+        question = Question.create(:title => title, :content => item.text, :user_id => 1)
+        question_id = question.id
+        puts "Create Question for #{question_id}"
+      else
+        puts question_id
+        Answer.create(:question_id => question_id, :user_id => 1, :content => item.text)
+        puts ".......... .......... add answer to #{question_id}"
+      end
     end
   end
 end
