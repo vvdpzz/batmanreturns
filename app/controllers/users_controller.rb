50a11f95a65f5330c668e00ac52f68a81829c1f4
xxd <xxdxxd51@hotmail.com>
2011-08-26 16:07:08 +0900

    1)user page: follower/following function done; 2)fixed duplicated relationship.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,16 @@
 class UsersController < ApplicationController
   def follow
     following_user = User.find params[:id]
-    puts following_user
     if following_user
       follower_id = current_user.id
       followed_id = params[:id]
       if not following_user.has_relationship(current_user.id)
         $redis.sadd("user:#{current_user.id}.follows", params[:id])
+        current_user.relationships.create(:follower_id => follower_id,:followed_id => followed_id)
       end
       if following_user.has_relationship(current_user.id)
         description = APP_CONFIG["notice_follow_user"]
         Resque.enqueue(NewFollowerQueue, followed_id, follower_id, current_user.realname, description)
-        in_relationship = User.where(:follower_id => follower_id,:followed_id => followed_id)
-        if in_relationship
-          current_user.relationships.create(:follower_id => follower_id,:followed_id => followed_id)
-        end
       end
     end
   end
@@ -26,8 +22,8 @@ class UsersController < ApplicationController
       followed_id = params[:id]
       if following_user.has_relationship(current_user.id)
         $redis.serm("user:#{current_user.id}.follows", params[:id])
+        current_user.relationships.update_arributes(:following => false)
       end
-      current_user.relationships.update_arributes(:following => false)
     end
   end
   
